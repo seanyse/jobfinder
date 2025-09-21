35455ff33667b8094d87ac366637180b648317ff
Auska Wang <134457403+tastyshowers@users.noreply.github.com>
2025-09-21 19:52:19 -0400

    added ability to edit delete job postings and clarified filter fields
    note: recruiters and delete edit their own jobs,

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='jobs.index'),
     path('create/', views.create_job, name='jobs.create_job'),
+    path('<int:job_id>/edit/', views.edit_job, name='jobs.edit_job'),
+    path('<int:job_id>/delete/', views.delete_job, name='jobs.delete_job'),
 ]
