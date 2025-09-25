7e65fa7b3dca01de34edf553848d45a22fc886a5
ShreyasB1 <93990742+ShreyasB1@users.noreply.github.com>
2025-09-25 15:47:06 -0400

    Update urls.py

--- jobs/urls.py
+++ jobs/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('<int:job_id>/', views.job_detail, name='jobs.detail'),
     path('<int:job_id>/edit/', views.edit_job, name='jobs.edit_job'),
     path('<int:job_id>/delete/', views.delete_job, name='jobs.delete_job'),
-    path('map/', views.job_map, name='jobs.map'),
-    path('api/jobs-geo/', views.jobs_geo_api, name='jobs.geo'),
+    
         
     # Application URLs
     path('<int:job_id>/apply/', views.apply_to_job, name='jobs.apply'),
